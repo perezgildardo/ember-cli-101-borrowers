e09db2f03f6aafa00a5b61dcc28377e54f46c84c
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-01-18 21:57:57 -0500

    Validate description presence in articles/new controller

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    function() {
+      return !Ember.isEmpty(this.get('model.description'));
+    }
+  ),
+
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        return true;
+      } else {
+        this.set('errorMessage', "You have to fill in the article's description.");
+        return false;
+      }
+    }
+  }
+});
