a7affe1a0b484da4dec9cd789d780d8501ce77d7
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-01-18 23:35:21 -0500

    Discard unsaved dirty changes on friends

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,13 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
+  deactivate: function() {
+    var model = this.modelFor('friends/new');
+
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
 
   actions: {
     save: function() {
